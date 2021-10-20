e5331bf6f241798a4dc95354d811fa96104f7580
Andrew Hunn <ahunn@umich.edu>
2021-10-20 15:08:12 -0400

    update tags to allow read only or authentication
    staff or superuser required for tag create

--- app/project/tests/test_tags_api.py
+++ app/project/tests/test_tags_api.py
@@ -19,27 +19,8 @@ class PublicTagsApiTests(TestCase):
     def setUp(self):
         self.client = APIClient()
 
-    def test_login_required(self):
-        """Test that login is required for retrieving tags"""
-        res = self.client.get(TAGS_URL)
-
-        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
-
-
-class PrivateTagsApiTests(TestCase):
-    """Test the authorized user tags API"""
-
-    def setUp(self):
-        self.user = get_user_model().objects.create_user(
-            'test',
-            'test@csdt.org',
-            'password123'
-        )
-        self.client = APIClient()
-        self.client.force_authenticate(self.user)
-
     def test_retrieve_tags(self):
-        """Test retrieving tags"""
+        """Test the retrieved tag list API for public users """
         Tag.objects.create(
             name="CSnap",
             description="Some sort of description"
@@ -53,3 +34,70 @@ class PrivateTagsApiTests(TestCase):
         serializer = TagSerializer(tags, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+
+    def test_create_tag_login_required(self):
+        """Test that login is required for retrieving tags"""
+        payload = {'name': 'CSDT', 'description': 'Text'}
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateTagsApiTests(TestCase):
+    """Test the authorized user tags API"""
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_superuser(
+            'test',
+            'test@csdt.org',
+            'password123'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_tags(self):
+        """Test retrieving tags for authenticated users"""
+        Tag.objects.create(
+            name="CSnap",
+            description="Some sort of description"
+        )
+        Tag.objects.create(
+            name="High",
+            description="High School projects"
+        )
+        res = self.client.get(TAGS_URL)
+        tags = Tag.objects.all().order_by('-name')
+        serializer = TagSerializer(tags, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_create_tag_successful(self):
+        """Test creating a new tag"""
+        payload = {'name': 'Quilting', 'description': 'Quilting projects'}
+        self.client.post(TAGS_URL, payload)
+
+        exists = Tag.objects.filter(
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        """Test creating a new tag with invalid payload"""
+        payload = {'name': '', 'description': ''}
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_tag_not_staff(self):
+        """Test with non staff users not allowed to create tags"""
+        user2 = get_user_model().objects.create_user(
+            'test2',
+            'test2@csdt.org',
+            'password1233'
+        )
+        client = APIClient()
+        client.force_authenticate(user2)
+        payload = {'name': 'CSDT', 'description': 'Test'}
+        res = client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
--- app/project/views.py
+++ app/project/views.py
@@ -1,18 +1,29 @@
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from django.core.exceptions import PermissionDenied
 
 from core.models import Tag
 from project import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet,
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     """Manage tags in the database"""
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
 
     def get_queryset(self):
         """Return objects for only authenticated users"""
         return self.queryset.order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new tag only if user is staff"""
+        if not self.request.user.is_staff:
+            raise PermissionDenied()
+
+        serializer.save()
